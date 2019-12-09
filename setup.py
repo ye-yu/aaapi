68592e862ddf8998e3f5a557bf7475d908907329
ye-yu <rafolwen98@gmail.com>
2019-12-10 07:15:11 +0800

    Increase python version requirement

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [ ]
 setup(
     author="Ye Yu",
     author_email='rafolwen98@gmail.com',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
